08a238b4d600a97a470be104a47ba3edc57bdcc5
vedantsrivastava42 <vedantsrivastava42@gmail.com>
2025-08-04 01:38:14 +0530

    changed env variables

--- BACKEND/controllers/Profile.js
+++ BACKEND/controllers/Profile.js
@@ -143,7 +143,7 @@ exports.updateDisplayPicture = async (req, res) => {
     }
 	const uploadDetails = await uploadImageToCloudinary(
 		image,
-		process.env.FOLDER_NAME
+		process.env.CLOUDINARY_FOLDER_NAME
 	);
 	console.log(uploadDetails);
 
--- BACKEND/utils/imageUploader.js
+++ BACKEND/utils/imageUploader.js
@@ -1,15 +1,20 @@
 const cloudinary = require('cloudinary').v2
 
 
-exports.uploadImageToCloudinary  = async (file, folder, height, quality) => {
-    const options = {folder};
+exports.uploadImageToCloudinary  = async (file, folder, height, quality, isVideo = false) => {
+    // Use env variable if folder is not provided
+    let targetFolder = folder;
+    if (!targetFolder) {
+        targetFolder = isVideo ? process.env.CLOUDINARY_FOLDER_VIDEO : process.env.CLOUDINARY_FOLDER_NAME;
+    }
+    const options = { folder: targetFolder };
     if(height) {
         options.height = height;
     }
     if(quality) {
         options.quality = quality;
     }
-    options.resource_type = "auto";
+    options.resource_type = isVideo ? "video" : "auto";
 
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
